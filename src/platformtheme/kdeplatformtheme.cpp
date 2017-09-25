059369d6b6a9a99538c175a3c53058cbb54e8abc
Antonio Rojas <arojas@archlinux.org>
2017-09-25 20:27:11 +0200

    Use QQuickStyle to set the QQC2 style
    Use QQuickStyle::setStyle instead of an environment variable to set the QQC2 desktop style. This prevent the style from being inherited by
    plasmashell (or krunner) child processes, which makes QGuiApplications crash. The QQC1 style still needs to be set via an environment variable, so
    we explicitely unset it for non-QApplications to prevent them from crashing.
    
    Differential Revision: https://phabricator.kde.org/D7953
    BUG: 384481

--- src/platformtheme/kdeplatformtheme.cpp
+++ src/platformtheme/kdeplatformtheme.cpp
@@ -43,6 +43,7 @@
 #include <QDBusConnection>
 #include <QDBusConnectionInterface>
 #include <QDebug>
+#include <QtQuickControls2/QQuickStyle>
 
 #include <kiconengine.h>
 #include <kiconloader.h>
@@ -394,15 +395,19 @@ QPlatformMenuBar *KdePlatformTheme::createPlatformMenuBar() const
 //force QtQuickControls2 to use the desktop theme as default
 void KdePlatformTheme::setQtQuickControlsTheme()
 {
-    //if the user has explicitly set something else, don't meddle
-    if (qEnvironmentVariableIsSet("QT_QUICK_CONTROLS_STYLE")) {
-        return;
-    }
-    //if the user is running only a QGuiApplication. Abort as this style is all about QWidgets and we know setting this will make it crash
+    //if the user is running only a QGuiApplication, explicitely unset the QQC1 desktop style and abort
+    //as this style is all about QWidgets and we know setting this will make it crash
     if (!qobject_cast<QApplication*>(qApp)) {
+        if (qgetenv("QT_QUICK_CONTROLS_1_STYLE").right(7) == "Desktop") {
+            qunsetenv("QT_QUICK_CONTROLS_1_STYLE");
+        }
         return;
     }
-    qputenv("QT_QUICK_CONTROLS_STYLE", "org.kde.desktop");
+    //if the user has explicitly set something else, don't meddle
+    if (!QQuickStyle::name().isEmpty()) {
+        return;
+    }
+    QQuickStyle::setStyle(QLatin1String("org.kde.desktop"));
 }
 
 #include "kdeplatformtheme.moc"
