KDE/plasma-integration

commit bfd41a95530f90ee8d44cbcfd1fa8c62978334a2

KDEPlatformFileDialog: Fix initial directory selection for remote files
Summary:
Previously KDEPlatformFileDialogHelper::selectFile() would change
options()->initialDirectory() unconditionally even if it was already
set by the QFileDialog code. Since Qt 5.7.1 it is no longer necessary
to derive initialDirectory from the selectFile() call. In fact it is
actuall harmful since it will now override the correct initial directory
that was set by Qt. Without this patch I got the following debug output:

```
KDEPlatformFileDialogHelper::setDirectory QUrl("sftp://server/home/alr48/cheri/build_sdk.sh")
KDEPlatformFileDialogHelper::setDirectory QUrl("sftp://server/home/alr48/cheri/build_sdk.sh")
KDEPlatformFileDialogHelper::selectFile QUrl("file:///home/alex/build_sdk.sh")
KDEPlatformFileDialogHelper::setDirectory QUrl("file:///home/alex/)
```
The final setDirectory() call is actually a call to
`setDirectory(options->initialDirectory())` which was set in `selectFile()`.

We now depend on Qt 5.9 so we can remove this code without a check for
version >= 5.7.1.

BUG: 374913

Test Plan: Remote directory is now opened correctly (tested with Qt 5.10.0)

Reviewers: #plasma, elvisangelaccio

Reviewed By: elvisangelaccio

Subscribers: ngraham, krzyc, anthonyfieroni, elvisangelaccio, graesslin, plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D4193

src/platformtheme/kdeplatformfiledialoghelper.cpp
@@ -409,11 +409,6 @@ QUrl KDEPlatformFileDialogHelper::directory() const
 void KDEPlatformFileDialogHelper::selectFile(const QUrl &filename)
 {
     m_dialog->selectFile(filename);
-
-    // Qt 5 at least <= 5.8.0 does not derive the directory from the passed url
-    // and set the initialDirectory option accordingly, also not for known schemes
-    // like file://, so we have to do it ourselves
-    options()->setInitialDirectory(m_dialog->directory());
 }
 
 void KDEPlatformFileDialogHelper::setDirectory(const QUrl &directory)

commit 22da1e661afdf7f05dbbc0ed0d59a49987e2a32a

File Dialog: fix testSelectUrl() again, i.e. selectUrl() should set the directory too
Summary:
This was fixed initially in commit 7bbbd93 (https://phabricator.kde.org/D3796)
when the unittest was added, and broken later in commit bfd41a9
(https://phabricator.kde.org/D4193, bug 374913) which says Qt takes care
of taking the initial directory when calling selectUrl(). I cannot see
that in the Qt code, where selectUrl only calls selectFile_sys.

This breaks bug 374913 again (initial directory for remote files), but the
right fix for that is https://codereview.qt-project.org/235473

Test Plan: unittest

Reviewers: arichardson, anthonyfieroni, elvisangelaccio, plasma-devel, broulik

Differential Revision: https://phabricator.kde.org/D14440

src/platformtheme/kdeplatformfiledialoghelper.cpp
@@ -270,6 +270,7 @@ KDEPlatformFileDialogHelper::~KDEPlatformFileDialogHelper()
 
 void KDEPlatformFileDialogHelper::initializeDialog()
 {
+    m_dialogInitialized = true;
     if (options()->testOption(QFileDialogOptions::ShowDirsOnly)) {
         m_dialog->deleteLater();
         m_dialog = new KDirSelectDialog(options()->initialDirectory());
@@ -286,7 +287,9 @@ void KDEPlatformFileDialogHelper::initializeDialog()
         } else {
             dialog->setWindowTitle(options()->windowTitle());
         }
-        setDirectory(options()->initialDirectory());
+        if (!m_directorySet) {
+            setDirectory(options()->initialDirectory());
+        }
         //dialog->setViewMode(options()->viewMode()); // don't override our options, fixes remembering the chosen view mode and sizes!
         dialog->setFileMode(options()->fileMode());
 
@@ -412,13 +415,21 @@ QUrl KDEPlatformFileDialogHelper::directory() const
 
 void KDEPlatformFileDialogHelper::selectFile(const QUrl &filename)
 {
+    // This is called once by QFileDialogPrivate::init -> QFileDialog::selectUrl -> QFileDialogPrivate::selectFile_sys
+    // and then again by selectFile in the QFileDialog constructor, with a wrong value for remote URLs, until the Qt 5.11.2 fix.
+#if QT_VERSION < QT_VERSION_CHECK(5, 12, 0)
+    if (m_fileSelected && !m_dialogInitialized)
+        return;
+#endif
     m_dialog->selectFile(filename);
+    m_fileSelected = true;
 }
 
 void KDEPlatformFileDialogHelper::setDirectory(const QUrl &directory)
 {
     if (!directory.isEmpty()) {
         m_dialog->setDirectory(directory);
+        m_directorySet = true;
     }
 }
 

src/platformtheme/kdeplatformfiledialoghelper.h
@@ -80,6 +80,9 @@ private Q_SLOTS:
 private:
     void restoreSize();
     KDEPlatformFileDialogBase *m_dialog;
+    bool m_directorySet = false;
+    bool m_fileSelected = false;
+    bool m_dialogInitialized = false;
 };
 
 #endif // KDEPLATFORMFILEDIALOGHELPER_H